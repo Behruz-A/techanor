9c40b7cee3a68666c9b11a10f9d3d03080efb565
Behruz-A <behruzanorboev1@gmail.com>
2026-08-10 01:45:17 +0900

    feat: developed login and signup endpoints of memberController

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,40 @@
 import { T } from "../libs/types/common";
 import { Request, Response } from "express";
+import { LoginInput, MemberInput } from "../libs/types/member";
+import MemberService from "../models/Member.service";
+import Errors from "../libs/Error";
 
-// React
 const memberController: T = {};
+
+const memberService = new MemberService();
+memberController.signup = async (req: Request, res: Response) => {
+  try {
+    console.log("signup");
+    const input: MemberInput = req.body,
+      result = await memberService.signup(input);
+    //TODO:  tokens AUTHENTICATION
+
+    res.json({ member: result });
+  } catch (err) {
+    console.log("Error, signup:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
+
+memberController.login = async (req: Request, res: Response) => {
+  try {
+    console.log("login ");
+    const input: LoginInput = req.body,
+      result = await memberService.login(input);
+    //TODO:  tokens AUTHENTICATION
+
+    res.json({ member: result });
+  } catch (err) {
+    console.log("Error, login:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
 
 export default memberController;
--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -4,6 +4,8 @@ import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
+
 const storeController: T = {};
 storeController.goHome = (req: Request, res: Response) => {
   try {
@@ -11,15 +13,6 @@ storeController.goHome = (req: Request, res: Response) => {
     res.send("Welcome to Home Page");
   } catch (err) {
     console.log("Error, goHome:", err);
-  }
-};
-
-storeController.getLogin = (req: Request, res: Response) => {
-  try {
-    console.log("getLogin");
-    res.send("Login Page");
-  } catch (err) {
-    console.log("Error, getLogin:", err);
   }
 };
 
@@ -32,18 +25,12 @@ storeController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-storeController.processLogin = async (req: Request, res: Response) => {
+storeController.getLogin = (req: Request, res: Response) => {
   try {
-    console.log("processLogin ");
-    const input: LoginInput = req.body;
-
-    const memberService = new MemberService();
-    const result = await memberService.processLogin(input);
-
-    res.send(result);
+    console.log("getLogin");
+    res.send("Login Page");
   } catch (err) {
-    console.log("Error, processLogin:", err);
-    res.send(err);
+    console.log("Error, getLogin:", err);
   }
 };
 
@@ -53,8 +40,8 @@ storeController.processSignup = async (req: Request, res: Response) => {
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.STORE;
-    const memberService = new MemberService();
     const result = await memberService.processSignup(newMember);
+    //TODO:  sessions AUTHENTICATION
 
     res.send(result);
   } catch (err) {
@@ -62,4 +49,19 @@ storeController.processSignup = async (req: Request, res: Response) => {
     res.send(err);
   }
 };
+
+storeController.processLogin = async (req: Request, res: Response) => {
+  try {
+    console.log("processLogin ");
+    const input: LoginInput = req.body,
+      result = await memberService.processLogin(input);
+    //TODO:  sessions AUTHENTICATION
+
+    res.send(result);
+  } catch (err) {
+    console.log("Error, processLogin:", err);
+    res.send(err);
+  }
+};
+
 export default storeController;
