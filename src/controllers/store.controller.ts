03fb522c6a567f25be417e82ad3a5eb27642d63f
Behruz-A <behruzanorboev1@gmail.com>
2026-08-09 23:42:08 +0900

    feat: developed processLogin

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,7 +1,7 @@
 import { T } from "../libs/types/common";
 import { Request, Response } from "express";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const storeController: T = {};
@@ -32,19 +32,24 @@ storeController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-storeController.processLogin = (req: Request, res: Response) => {
+storeController.processLogin = async (req: Request, res: Response) => {
   try {
     console.log("processLogin ");
-    res.send("DONE");
+    const input: LoginInput = req.body;
+
+    const memberService = new MemberService();
+    const result = await memberService.processLogin(input);
+
+    res.send(result);
   } catch (err) {
-    console.log("Error, processLogin  :", err);
+    console.log("Error, processLogin:", err);
+    res.send(err);
   }
 };
 
 storeController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log("processSignup");
-    console.log("body:", req.body);
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.STORE;
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,7 @@
 import MemberModel from "../controllers/schema/Member.model";
 import { MemberType } from "../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/Error";
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 
 class MemberService {
   private readonly memberModel;
@@ -24,6 +24,20 @@ class MemberService {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATION_FAILED);
     }
   }
-}
 
+  public async processLogin(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        { memberNick: input.memberNick },
+        { memberNick: 1, memberPassword: 1 },
+      )
+      .exec();
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    const isMatch = input.memberPassword === member.memberPassword;
+    if (!isMatch)
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    return await this.memberModel.findById(member._id).exec();
+  }
+}
 export default MemberService;
